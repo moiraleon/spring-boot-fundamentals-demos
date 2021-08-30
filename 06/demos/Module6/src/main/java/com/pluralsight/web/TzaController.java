fedbd7d7aca14b5c6f9da9ea281a9d45fb6244a5
Moira Leon <moira.leon2@gmail.com>
2021-08-30 14:53:14 -0600

    RESTful Web Application

--- 06/demos/Module6/src/main/java/com/pluralsight/web/TzaController.java
+++ 06/demos/Module6/src/main/java/com/pluralsight/web/TzaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@RestController //adds @Controller and @ResponseBody
 @RequestMapping("/tza")
 public class TzaController {
     private ApplicationService applicationService;
